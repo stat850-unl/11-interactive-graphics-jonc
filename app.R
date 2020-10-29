27aa9e26784b374fbc1a47a00ee88aed3e2b77fe
Oliver Titus <ojamestitus@gmail.com>
2020-10-29 14:28:16 -0500

    susan's code

--- app.R
+++ app.R
@@ -20,32 +20,32 @@ ui <- fluidPage(
     titlePanel("New England Cocktails For Dummies"),
 
     fluidPage(
-        
+
         # Copy the chunk below to make a group of checkboxes
         checkboxGroupInput("checkGroup", label = h3("Drink Category"),
                            choices = list("Vodka" = 1, "Tequila" = 2, "Cocktail Classics" = 3),
                            selected = 1),
-        
-        
+
+
         hr(),
         fluidRow(column(3, verbatimTextOutput("value"))),
-        
+
         checkboxGroupInput("checkGroup", label = h3("Juice Type"),
                            choices = list("Pineapple" = 1, "Orange" = 2, "Apple" = 3),
                            selected = 1),
-        
-        
+
+
         hr(),
         fluidRow(column(3, verbatimTextOutput("value")))
-        
-        
-        
+
+
+
     )
 )
 
 
-    
-    
+
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
@@ -64,13 +64,44 @@ server <- function(input, output) {
 shinyApp(ui = ui, server = server)
 
 # fluidPage(
-# 
+#
 #     # Copy the chunk below to make a group of checkboxes
 #     checkboxGroupInput("checkGroup", label = h3("Checkbox group"),
 #                        choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
 #                        selected = 1),
-# 
-# 
+#
+#
 #     hr(),
 #     fluidRow(column(3, verbatimTextOutput("value")))
-# 
+#
+# drink names with correct category and type of juice
+
+juice_search <- str_to_lower(input$juice) %>%
+    # paste("\b", ., "\b", sep = "") %>%
+    paste(collapse = "|")
+
+boston_cocktails %>%
+    filter(category %in% input$booze) %>%
+    filter(str_detect(ingredient, juice_search)) %>%
+    select(name)
+
+
+
+# --------------------------------
+
+
+# Take multiple juice options and look for any matches
+juice_search <- str_to_lower(input$juice) %>%
+    # paste("\b", ., "\b", sep = "") %>%
+    paste(collapse = "|")
+
+
+
+
+
+
+
+boston_cocktails %>%
+    filter(category %in% input$booze) %>%
+    filter(str_detect(ingredient, input$juice)) %>%
+    select(name)
